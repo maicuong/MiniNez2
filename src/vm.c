2e3d77d88cf927c9608affd0d9bc0f69587dfc02
shun-honda <shun.honda.suffix@gmail.com>
2015-08-26 14:21:18 +0900

    update mininez vm

--- src/vm.c
+++ src/vm.c
@@ -1,6 +1,132 @@
+#include <stdlib.h>
+
 #include "vm.h"
 
-long mininez_vm_execute(Context ctx, MiniNezInstruction *inst) {
+char *loadFile(const char *filename, size_t *length);
 
+void nez_PrintErrorInfo(const char *errmsg) {
+  fprintf(stderr, "%s\n", errmsg);
+  exit(EXIT_FAILURE);
+}
+
+Context nez_CreateParsingContext(const char *filename) {
+  Context ctx = (Context)malloc(sizeof(struct Context));
+  ctx->input_size = 0;
+  ctx->inputs = loadFile(filename, &ctx->input_size);
+  ctx->stack_pointer_base =
+      (StackEntry)malloc(sizeof(struct StackEntry) * CONTEXT_MAX_STACK_LENGTH);
+  ctx->stack_pointer = &ctx->stack_pointer_base[0];
+  ctx->stack_size = CONTEXT_MAX_STACK_LENGTH;
+  return ctx;
+}
+
+static inline void push_alt(Context ctx, long pos, MiniNezInstruction* jmp) {
+  ctx->stack_pointer->pos = pos;
+  ctx->stack_pointer->jmp = jmp;
+  ctx->stack_pointer++;
+}
+
+static inline void push_pos(Context ctx, long pos) {
+  (ctx->stack_pointer++)->pos = pos;
+}
+
+static inline StackEntry pop(Context ctx) {
+  return (--ctx->stack_pointer);
+}
+
+#define MININEZ_USE_INDIRECT_THREADING 1
+
+long mininez_vm_execute(Context ctx, MiniNezInstruction *inst) {
+  register const char *cur = ctx->inputs;
+  register int failflag = 0;
+  register const MiniNezInstruction *pc;
+  register long pos = 0;
+
+#ifdef MININEZ_USE_SWITCH_CASE_DISPATCH
+#define DISPATCH_NEXT()         goto L_vm_head
+#define DISPATCH_START(PC) L_vm_head:;switch (*PC++) {
+#define DISPATCH_END()     default: ABORT(); }
+#define LABEL(OP)          case OP
+#else
+#define LABEL(OP)          MININEZ_OP_##OP
+    static const void *__table[] = {
+#define DEFINE_TABLE(OP) &&LABEL(OP),
+        MININEZ_IR_EACH(DEFINE_TABLE)
+#undef DEFINE_TABLE
+    };
+#define DISPATCH_START(PC) DISPATCH_NEXT()
+#define DISPATCH_END()     ABORT();
+
+#if defined(MININEZ_USE_INDIRECT_THREADING)
+#define DISPATCH_NEXT() goto *__table[(pc++)->op]
+#define fail() \
+  StackEntry top = (--ctx->stack_pointer);\
+  pos = top->pos;\
+  pc = top->jmp;\
+  goto *__table[pc->op];
+#else
+#error please specify dispatch method
+#endif
+#endif
+
+#define OP_CASE_(OP) LABEL(OP):
+
+#if MININEZ_DEBUG == 1
+#define OP_CASE(OP) OP_CASE_(OP); fprintf(stderr, "%s\n", get_opname(pc->op);
+#else
+#define OP_CASE(OP) OP_CASE_(OP)
+#endif
+
+  pc = inst + 1;
+
+  OP_CASE(Iexit) {
+    return pc->arg;
+  }
+  OP_CASE(Inop) {
+    DISPATCH_NEXT();
+  }
+  OP_CASE(Ifail) {
+    fail();
+  }
+  OP_CASE(Ialt) {
+    push_alt(ctx, pos, pc+pc->arg);
+  }
+  OP_CASE(Isucc) {
+    pop();
+  }
+  OP_CASE(Ijump) {
+
+  }
+  OP_CASE(Icall) {
+
+  }
+  OP_CASE(Iret) {
+
+  }
+  OP_CASE(Ipos) {
+
+  }
+  OP_CASE(Iback) {
+
+  }
+  OP_CASE(Iskip) {
+
+  }
+  OP_CASE(Ibyte) {
+
+  }
+  OP_CASE(Iany) {
+
+  }
+  OP_CASE(Istr) {
+
+  }
+  OP_CASE(Iset) {
+
+  }
+  return 0;
+}
+
+int main(void) {
   return 0;
 }
--- src/vm.h
+++ src/vm.h
@@ -1,7 +1,35 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+
 #ifndef VM_H
 #define VM_H
 
 #define MININEZ_DEBUG 0
+
+#define MININEZ_IR_EACH(OP)\
+	OP(Iexit)\
+	OP(Inop)\
+	OP(Ifail)\
+	OP(Ialt)\
+	OP(Isucc)\
+	OP(Ijump)\
+	OP(Icall)\
+	OP(Iret)\
+	OP(Ipos)\
+	OP(Iback)\
+	OP(Iskip)\
+	OP(Ibyte)\
+	OP(Iany)\
+	OP(Istr)\
+	OP(Iset)
+
+enum nezvm_opcode {
+#define DEFINE_ENUM(NAME) MININEZ_OP_##NAME,
+  MININEZ_IR_EACH(DEFINE_ENUM)
+#undef DEFINE_ENUM
+  MININEZ_OP_ERROR = -1
+};
 
 typedef struct MiniNezInstruction {
 	unsigned short op : 5;
@@ -10,7 +38,7 @@ typedef struct MiniNezInstruction {
 
 struct StackEntry {
   long pos;
-  unsigned long jmp;
+  MiniNezInstruction* jmp;
 };
 
 struct Context {
@@ -18,11 +46,29 @@ struct Context {
   size_t input_size;
 
   size_t stack_size;
-  union StackEntry* stack_pointer;
-  union StackEntry* stack_pointer_base;
+  struct StackEntry* stack_pointer;
+  struct StackEntry* stack_pointer_base;
 };
+#define CONTEXT_MAX_STACK_LENGTH 1024
 
 typedef struct StackEntry* StackEntry;
 typedef struct Context* Context;
+
+static const char *get_opname(uint8_t opcode) {
+  switch (opcode) {
+#define OP_DUMPCASE(OP) \
+  case MININEZ_OP_##OP:   \
+    return "" #OP;
+    MININEZ_IR_EACH(OP_DUMPCASE);
+  default:
+    fprintf(stderr, "%d\n", opcode);
+    assert(0 && "UNREACHABLE");
+    break;
+#undef OP_DUMPCASE
+  }
+  return "";
+}
+
+void nez_PrintErrorInfo(const char *errmsg);
 
 #endif
